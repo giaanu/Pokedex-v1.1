f21cb5467af10e368e1a3a636713932591704972
Gianluca Zarrelli <g.zarrelli96@gmail.com>
2025-12-13 00:55:36 -0300

    Initial Commit

---
+++ Pokedex-v1.1/Pokedex_v1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Pokedex_v1_1App.swift
+//  Pokedex-v1.1
+//
+//  Created by Gianluca Zarrelli on 13/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pokedex_v1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
